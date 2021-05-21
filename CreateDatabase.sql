dbf96bd53b6971fb172d2ab7c28e21b4f95a7052
Тимофей <70803605+Tyrlock@users.noreply.github.com>
2021-05-21 10:28:59 +0300

    Add files via upload

---
+++ CreateDatabase.sql
@@ -0,0 +1,299 @@
+create database Restaurateur
+Go
+
+use Restaurateur
+--Создание таблицы Roles
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Roles](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [varchar](50) NOT NULL,
+ CONSTRAINT [PK_Access] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+INSERT INTO [dbo].[Roles]
+           ([Name])
+     VALUES
+           ('Administrator')
+GO
+
+INSERT INTO [dbo].[Roles]
+           ([Name])
+     VALUES
+           ('User')
+GO
+
+--Создание таблицы Users
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Users](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](100) NOT NULL,
+	[Phone] [varchar](100) NULL,
+	[Email] [varchar](100) NULL,
+	[Password] [nchar](100) NOT NULL,
+	[RoleId] [int] NULL,
+ CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Users]  WITH CHECK ADD  CONSTRAINT [FK_Users_Roles] FOREIGN KEY([RoleId])
+REFERENCES [dbo].[Roles] ([Id])
+GO
+
+ALTER TABLE [dbo].[Users] CHECK CONSTRAINT [FK_Users_Roles]
+GO
+
+--Создание таблицы Tables
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Tables](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](50) NOT NULL,
+	[TotalSeats] [int] NOT NULL,
+ CONSTRAINT [PK_Tables] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+INSERT INTO [dbo].[Tables]
+           ([Name]
+           ,[TotalSeats])
+     VALUES
+           ('1','2')
+GO
+
+INSERT INTO [dbo].[Tables]
+           ([Name]
+           ,[TotalSeats])
+     VALUES
+           ('2','4')
+GO
+
+INSERT INTO [dbo].[Tables]
+           ([Name]
+           ,[TotalSeats])
+     VALUES
+           ('3','5')
+GO
+
+INSERT INTO [dbo].[Tables]
+           ([Name]
+           ,[TotalSeats])
+     VALUES
+           ('4','2')
+GO
+
+INSERT INTO [dbo].[Tables]
+           ([Name]
+           ,[TotalSeats])
+     VALUES
+           ('5','3')
+GO
+
+INSERT INTO [dbo].[Tables]
+           ([Name]
+           ,[TotalSeats])
+     VALUES
+           ('6','4')
+GO
+
+INSERT INTO [dbo].[Tables]
+           ([Name]
+           ,[TotalSeats])
+     VALUES
+           ('7','8')
+GO
+
+--Создание таблицы Bookings
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Bookings](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[ClientName] [nvarchar](50) NOT NULL,
+	[DataTimeBooking] [datetimeoffset](7) NOT NULL,
+	[Seats] [int] NOT NULL,
+	[TableId] [int] NOT NULL,
+	[ClientPhoneNumber] [nvarchar](50) NOT NULL,
+	[UpdatedBy] [int] NOT NULL,
+	[UpdatedDate] [datetimeoffset](7) NOT NULL,
+ CONSTRAINT [PK_Bookings] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Bookings] ADD  CONSTRAINT [DF_Bookings_UpdatedDate]  DEFAULT (sysdatetimeoffset()) FOR [UpdatedDate]
+GO
+
+ALTER TABLE [dbo].[Bookings]  WITH CHECK ADD  CONSTRAINT [FK_Bookings_Tables] FOREIGN KEY([TableId])
+REFERENCES [dbo].[Tables] ([Id])
+GO
+
+ALTER TABLE [dbo].[Bookings] CHECK CONSTRAINT [FK_Bookings_Tables]
+GO
+
+ALTER TABLE [dbo].[Bookings]  WITH CHECK ADD  CONSTRAINT [FK_Bookings_Users] FOREIGN KEY([UpdatedBy])
+REFERENCES [dbo].[Users] ([Id])
+GO
+
+ALTER TABLE [dbo].[Bookings] CHECK CONSTRAINT [FK_Bookings_Users]
+GO
+
+--Создание таблицы Reviews
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Reviews](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[CreatedBy] [int] NOT NULL,
+	[Description] [nvarchar](max) NOT NULL,
+	[RecepientId] [int] NOT NULL,
+ CONSTRAINT [PK_Reviews] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Reviews]  WITH CHECK ADD  CONSTRAINT [FK_Reviews_Users] FOREIGN KEY([CreatedBy])
+REFERENCES [dbo].[Users] ([Id])
+GO
+
+ALTER TABLE [dbo].[Reviews] CHECK CONSTRAINT [FK_Reviews_Users]
+GO
+
+ALTER TABLE [dbo].[Reviews]  WITH CHECK ADD  CONSTRAINT [FK_Reviews_Users1] FOREIGN KEY([RecepientId])
+REFERENCES [dbo].[Users] ([Id])
+GO
+
+ALTER TABLE [dbo].[Reviews] CHECK CONSTRAINT [FK_Reviews_Users1]
+GO
+
+
+
+--Создание таблицы Categories
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Categories](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](50) NOT NULL,
+ CONSTRAINT [PK_Categories] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+INSERT INTO [dbo].[Categories]
+           ([Name])
+     VALUES
+           ('Drinks')
+GO
+
+INSERT INTO [dbo].[Categories]
+           ([Name])
+     VALUES
+           ('Alcohol')
+GO
+
+INSERT INTO [dbo].[Categories]
+           ([Name])
+     VALUES
+           ('HotDishes')
+GO
+
+INSERT INTO [dbo].[Categories]
+           ([Name])
+     VALUES
+           ('Salads')
+GO
+
+INSERT INTO [dbo].[Categories]
+           ([Name])
+     VALUES
+           ('Juices')
+GO
+
+--Создание таблицы Menu
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Menu](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[ProductName] [nvarchar](max) NOT NULL,
+	[Cost] [int] NULL,
+	[Description] [nvarchar](max) NOT NULL,
+	[CategoryId] [int] NOT NULL,
+	[Image] [image] NULL,
+	[UpdatedBy] [int] NOT NULL,
+	[UpdatedDate] [datetimeoffset](7) NOT NULL,
+ CONSTRAINT [PK_Menu] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Menu] ADD  CONSTRAINT [DF_Menu_UpdatedDate]  DEFAULT (sysdatetimeoffset()) FOR [UpdatedDate]
+GO
+
+ALTER TABLE [dbo].[Menu]  WITH CHECK ADD  CONSTRAINT [FK_Menu_Categories] FOREIGN KEY([CategoryId])
+REFERENCES [dbo].[Categories] ([Id])
+GO
+
+ALTER TABLE [dbo].[Menu] CHECK CONSTRAINT [FK_Menu_Categories]
+GO
+
+ALTER TABLE [dbo].[Menu]  WITH CHECK ADD  CONSTRAINT [FK_Menu_Users] FOREIGN KEY([UpdatedBy])
+REFERENCES [dbo].[Users] ([Id])
+GO
+
+ALTER TABLE [dbo].[Menu] CHECK CONSTRAINT [FK_Menu_Users]
+GO
+
+
+
+
